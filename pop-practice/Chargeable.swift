6d0ebbcd5dc6a523fd667cc72ab27939f7331aee
Grumpy <sewoong2890@gmail.com>
2022-04-30 10:46:12 +0900

    [refactor]: Chargeable 프로토콜 이동

--- pop-practice/Chargeable.swift
+++ pop-practice/Chargeable.swift
@@ -1,2 +1,6 @@
 import Foundation
 
+protocol Chargeable {
+    var maximumWattPerHour: WattPerHour { get set }
+    func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour
+}
--- pop-practice/main.swift
+++ pop-practice/main.swift
@@ -3,7 +3,4 @@ import Foundation
 typealias Watt = Int
 typealias WattPerHour = Int
 
-protocol Chargeable {
-    var maximumWattPerHour: WattPerHour { get set }
-    func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour
-}
+
