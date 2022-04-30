7ccec9bae770b4879983b2fdced7d3a26a44b09c
Grumpy <sewoong2890@gmail.com>
2022-04-30 10:48:05 +0900

    [feat]: 충전기 인스턴스 생성 및 충전

--- pop-practice/main.swift
+++ pop-practice/main.swift
@@ -3,4 +3,11 @@ import Foundation
 typealias Watt = Int
 typealias WattPerHour = Int
 
+let appleWatchCharger = Charger(maximumWattPerHour: 5)
+let iphoneCharger = Charger(maximumWattPerHour: 18)
+let ipadCharger = Charger(maximumWattPerHour: 30)
+let macBookChargerWith96Watt = Charger(maximumWattPerHour: 96)
+let macBookChargerWith106Watt = Charger(maximumWattPerHour: 106)
 
+var myMacBook = MacBook(allowedChargeWatt: 60, currentBatteryCapacity: 10)
+myMacBook.chargeBattery(charger: macBookChargerWith96Watt)
