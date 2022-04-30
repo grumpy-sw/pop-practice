fccb0d7c568980a601c3b959f20707383a07944f
Grumpy <sewoong2890@gmail.com>
2022-04-30 10:38:58 +0900

    [feat]: MacBook 구조체 구현

--- pop-practice/MacBook.swift
+++ pop-practice/MacBook.swift
@@ -1,3 +1,18 @@
 import Foundation
 
-
+struct MacBook {
+    let allowedChargeWatt: WattPerHour
+    var currentBatteryCapacity: WattPerHour
+    let maximumBatteryCapacity: WattPerHour = 100
+    
+    mutating func chargeBattery(charger: Chargeable) {
+        var hours: Int = 0
+        while currentBatteryCapacity < maximumBatteryCapacity {
+            currentBatteryCapacity += charger.convert(chargeableWattPerHour: allowedChargeWatt)
+            hours += 1
+        }
+        
+        currentBatteryCapacity = maximumBatteryCapacity
+        print("완전히 충전하기까지 걸린 시간: \(hours)시간")
+    }
+}
